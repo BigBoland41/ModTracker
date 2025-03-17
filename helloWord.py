7f859135ec6bb1d9e1d9ed428f91bf1818bb958b
Stephen Nuttall <stephenln99@gmail.com>
2025-03-16 23:41:47 -0400

    Made new helloWorld.py with PyQt6
    Made a new hello world program with PyQt6, now that we're switching away from GTK. Currently, running the file just opens a small window with nothing in it.

--- helloWord.py
+++ helloWord.py
@@ -1,22 +1,15 @@
-# Load Gtk
-import gi
-gi.require_version('Gtk', '4.0')
-from gi.repository import Gtk
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
-# When the application is launched…
-def on_activate(app):
-    # … create a new window…
-    win = Gtk.ApplicationWindow(application=app)
-    # … with a button in it…
-    btn = Gtk.Button(label='Hello, World!')
-    # … which closes the window when clicked
-    btn.connect('clicked', lambda x: win.close())
-    win.set_child(btn)
-    win.present()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-# Create a new application
-app = Gtk.Application(application_id='com.example.GtkApplication')
-app.connect('activate', on_activate)
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
 
-# Run the application
-app.run(None)
+if __name__ == "__main__":
+    main()
