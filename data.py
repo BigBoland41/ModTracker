51bd592158c173eedbcc6249e2ec4508d8103ce6
Andrew <andrewboland0@gmail.com>
2025-03-18 17:03:19 -0400

    Update data.py

--- data.py
+++ data.py
@@ -9,17 +9,47 @@ ID = 432 #game ID for minecraft
 headers = {
     "X-Api-Key": CFKEY
 }
+# just here to test the api call
+def test(): 
+    response = requests.get(URL + "games", headers=headers)
 
-response = requests.get(URL + "games", headers=headers)
+    print("Status Code:", response.status_code)
+    print("Response Text:", response.text)  # This helps see if CurseForge returned anything
 
-print("Status Code:", response.status_code)
-print("Response Text:", response.text)  # This helps see if CurseForge returned anything
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            print(data)
+        except requests.exceptions.JSONDecodeError:
+            print("Error: Response is empty or not JSON format.")
+    else:
+        print(f"Error {response.status_code}: {response.reason}")
 
-if response.status_code == 200:
-    try:
-        data = response.json()
-        print(data)
-    except requests.exceptions.JSONDecodeError:
-        print("Error: Response is empty or not JSON format.")
-else:
-    print(f"Error {response.status_code}: {response.reason}")
+
+def search_mod(modName, gameID):
+    url = f"{URL}mods/search"
+    params = {"gameId": gameID, "searchFilter": modName}
+
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        mods = response.json().get("data", [])  # Extracts the 'data' list
+        
+        # Extract only Mod Name and Mod ID
+        results = [(mod["id"], mod["name"]) for mod in mods]
+        return results
+    else:
+        print(f"Error {response.status_code}: {response.text}")
+        return None
+    
+def mod_info(modID):
+    url = f"{url}mods/{modID}"
+
+
+
+if __name__ == "__main__":
+    results = search_mod("weed", ID)
+    if(results != None):
+        for id, name in results:
+            print(f"Mod ID: {id} - name: {name}")
+    else:
+        print("got noting")
